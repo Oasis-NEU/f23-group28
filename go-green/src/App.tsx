ceedeab3011cc9b6d57b0d2a0ddd9f4f187c87df
Christianfitz22 <51420782+Christianfitz22@users.noreply.github.com>
2023-11-05 13:37:35 -0500

    Pushed mostly working task backend functionality
    By integrating with supabase, this code can now do the following.
    Recognize when a user logged in on the same day, and display the tasks already generated for that user.
    Recognize when the user logged in on a previous day, generate new tasks, display them, and record all new data for the user's new day.

--- go-green/src/App.tsx
+++ go-green/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { supabase } from "./supabase";
 import { useState, useEffect } from "react";
@@ -8,10 +7,79 @@ function App() {
 
 
   const [tasks, setTasks] = useState<any[]>([]);
+  const [users, setUsers] = useState<any[]>([]);
 
   useEffect(() => {
       // our fetch function
-      async function fetchTasks() {
+      
+      const currentTaskJSON: number[] = [];
+
+      async function fetchUser() {
+        const {data, error} = await supabase.from("User Info").select("*");
+
+        if (data) {
+            const todayDate = new Date();
+            var targetIndex = -1;
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].username === "Christianfitz") {
+                    targetIndex = i;
+                    break;
+                }
+            }
+            if (targetIndex === -1) {
+                console.log("Username not found in test function.");
+            } else {
+                const lastDateString = data[targetIndex].day_last_accessed;
+                //console.log(lastDateString);
+                const lastDate = new Date(lastDateString);
+                lastDate.setDate(lastDate.getDate());
+
+                //console.log(lastDate.toLocaleString());
+                //console.log(todayDate.toLocaleString());
+
+                //console.log(todayDate.getDate() + " and " + lastDate.getDate())
+                //console.log(todayDate.getMonth() + " and " + lastDate.getMonth())
+                //console.log(todayDate.getFullYear() + " and " + lastDate.getFullYear())
+
+                if (todayDate.getDate() === lastDate.getDate() && todayDate.getMonth() === lastDate.getMonth() && todayDate.getFullYear() === lastDate.getFullYear()) {
+                    //console.log("date was found to match");
+                    // fetch tasks that are already there
+                    const taskNumbers = data[targetIndex].current_tasks;
+                    fetchGivenTasks(taskNumbers);
+                } else {
+                    //console.log("date was found not to match.");
+                    fetchRandomTasks();
+                    // set last accessed day for this user to today
+                    const currentDayString = todayDate.getFullYear() + "-" + (todayDate.getMonth()+1) + "-" + todayDate.getDate();
+                    const { error } = await supabase.from('User Info').update({day_last_accessed: currentDayString}).eq('username', "Christianfitz");
+                    // record the current task list as their current tasks for today
+                    console.log("updating task numbers: " + currentTaskJSON);
+                    await supabase.from("User Info").update({current_tasks: currentTaskJSON}).eq('username', "Christianfitz");
+                    // set their completed tasks to be empty
+                    await supabase.from("User Info").update({completed_tasks: []}).eq('username', "Christianfitz");
+                }
+            }
+        } else {
+            console.log(error);
+        }
+      }
+      
+      async function fetchGivenTasks(taskNumbers: any[]) {
+        const {data, error} = await supabase.from("Task List").select("*");
+        if (data) {
+            const taskSubset = [];
+
+            for (let i = 0; i < taskNumbers.length; i++) {
+                taskSubset.push(data[taskNumbers[i]]);
+            }
+
+            setTasks(taskSubset);
+        } else {
+            console.log(error);
+        }
+      }
+
+      async function fetchRandomTasks() {
           const {data, error} = await supabase.from("Task List").select("*");
           if (data) {
               const tasksSubset = [];
@@ -21,6 +89,9 @@ function App() {
                   if (usedIndexes.indexOf(randomIndex) === -1) {
                       tasksSubset.push(data[randomIndex]);
                       usedIndexes.push(randomIndex);
+                      currentTaskJSON.push(randomIndex);
+                      console.log("item pushed");
+                      console.log(currentTaskJSON);
                   }
               }
               setTasks(tasksSubset);
@@ -29,7 +100,7 @@ function App() {
           }
       }
 
-      fetchTasks();
+      fetchUser();
   }, []); // on the first page load
 
   return (
@@ -37,10 +108,10 @@ function App() {
           <h1>Task List</h1>
           <ul>
               {tasks ? (
-                  tasks.map((tasks) => {
+                  tasks.map((task) => {
                       return (
                           //<li key={tasks.name}></li>
-                          <li key={tasks.Index}>{tasks.Name}: {tasks.Description} = +{tasks.Points} Points</li>
+                          <li key={task.Index}>{task.Name}: {task.Description} = +{task.Points} Points</li>
                       )
                   })) : (
                       <p>Loading...</p>
@@ -49,26 +120,6 @@ function App() {
           </ul>
       </div>
   )
-
-  //return <p>App</p>;
-
-          /*    
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="./GoGreenLogo.png" alt="GoGreen Logo"/>
-        <p>
-          Enter your username, or enter a new one.
-        </p>
-        <input type="text" name="UsernameInput" size={30} required></input>
-        <button type="button" name="test">Enter Your Garden</button>
-        <p>
-          Invalid username.
-        </p>
-      </header>
-    </div>
-  );
-  */
 }
 
 export default App;
