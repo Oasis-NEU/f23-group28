9f259b5f0b538f45c1074c7904af4df1120c5b3c
Miya-Guan <guan.yit@northeastern.edu>
2023-11-12 13:36:34 -0500

    Update App.tsx

--- go-green/src/App.tsx
+++ go-green/src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+
+ const [userName, setUserName] = useState(""); 
+ 
+
+ return (
     <div className="App">
       <header className="App-header">
         <img src="./GoGreenLogo.png" alt="GoGreen Logo"/>
         <p>
           Enter your username, or enter a new one.
         </p>
-        <input type="text" name="UsernameInput" size={30} required></input>
-        <button type="button" name="test">Enter Your Garden</button>
+        <input type="text" name="UsernameInput" size={30} onChange={(e) => setUserName(e.target.value)} value={userName} required></input>
+        <button type="button" name="test" onClick={() => console.log(userName)}>Enter Your Garden</button>
         <p>
           Invalid username.
         </p>
