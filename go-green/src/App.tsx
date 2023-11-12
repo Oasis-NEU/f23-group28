92e84addfedbc0bddc202e2726a89ebf17117aee
Christianfitz22 <51420782+Christianfitz22@users.noreply.github.com>
2023-11-12 13:42:41 -0500

    Fixed task non-generation loop. Added username addition functionality.

--- go-green/src/App.tsx
+++ go-green/src/App.tsx
@@ -14,49 +14,59 @@ function App() {
       
       const currentTaskJSON: number[] = [];
 
-      async function fetchUser() {
+      async function fetchUser(givenUsername: string) {
         const {data, error} = await supabase.from("User Info").select("*");
 
         if (data) {
             const todayDate = new Date();
             var targetIndex = -1;
             for (let i = 0; i < data.length; i++) {
-                if (data[i].username === "Christianfitz") {
+                if (data[i].username === givenUsername) {
                     targetIndex = i;
                     break;
                 }
             }
             if (targetIndex === -1) {
-                console.log("Username not found in test function.");
+                console.log("Provided username not found in database.");
+                const currentDayString = todayDate.getFullYear() + "-" + (todayDate.getMonth()+1) + "-" + todayDate.getDate();
+                await supabase.from('User Info').insert({ username: givenUsername, day_last_accessed: currentDayString, completed_tasks: [], current_tasks: [], points: 0});
+                fetchRandomTasks();
+                await supabase.from("User Info").update({current_tasks: currentTaskJSON}).eq('username', givenUsername);
             } else {
                 const lastDateString = data[targetIndex].day_last_accessed;
-                //console.log(lastDateString);
-                const lastDate = new Date(lastDateString);
-                lastDate.setDate(lastDate.getDate());
+                console.log("date string: " + lastDateString);
 
-                //console.log(lastDate.toLocaleString());
-                //console.log(todayDate.toLocaleString());
+                let dateArray = lastDateString.split("-");
 
-                //console.log(todayDate.getDate() + " and " + lastDate.getDate())
-                //console.log(todayDate.getMonth() + " and " + lastDate.getMonth())
-                //console.log(todayDate.getFullYear() + " and " + lastDate.getFullYear())
+                const lastDay = parseInt(dateArray[2]);
+                const lastMonth = parseInt(dateArray[1]);
+                const lastYear = parseInt(dateArray[0]);
 
-                if (todayDate.getDate() === lastDate.getDate() && todayDate.getMonth() === lastDate.getMonth() && todayDate.getFullYear() === lastDate.getFullYear()) {
+                console.log(todayDate.getDate() + " and " + lastDay)
+                console.log((todayDate.getMonth()+1) + " and " + lastMonth)
+                console.log(todayDate.getFullYear() + " and " + lastYear)
+
+                //const testDate = new Date("2023-11-5");
+                //console.log("test: " + testDate.toDateString());
+                //console.log("test get: " + testDate.getMonth());
+
+                const taskNumbers = data[targetIndex].current_tasks;
+
+                if (todayDate.getDate() === lastDay && (todayDate.getMonth()+1) === lastMonth && todayDate.getFullYear() === lastYear && taskNumbers.length !== 0) {
                     //console.log("date was found to match");
                     // fetch tasks that are already there
-                    const taskNumbers = data[targetIndex].current_tasks;
                     fetchGivenTasks(taskNumbers);
                 } else {
-                    //console.log("date was found not to match.");
+                    console.log("date was found not to match.");
                     fetchRandomTasks();
                     // set last accessed day for this user to today
                     const currentDayString = todayDate.getFullYear() + "-" + (todayDate.getMonth()+1) + "-" + todayDate.getDate();
-                    const { error } = await supabase.from('User Info').update({day_last_accessed: currentDayString}).eq('username', "Christianfitz");
+                    const { error } = await supabase.from('User Info').update({day_last_accessed: currentDayString}).eq('username', givenUsername);
                     // record the current task list as their current tasks for today
                     console.log("updating task numbers: " + currentTaskJSON);
-                    await supabase.from("User Info").update({current_tasks: currentTaskJSON}).eq('username', "Christianfitz");
+                    await supabase.from("User Info").update({current_tasks: currentTaskJSON}).eq('username', givenUsername);
                     // set their completed tasks to be empty
-                    await supabase.from("User Info").update({completed_tasks: []}).eq('username', "Christianfitz");
+                    await supabase.from("User Info").update({completed_tasks: []}).eq('username', givenUsername);
                 }
             }
         } else {
@@ -100,7 +110,7 @@ function App() {
           }
       }
 
-      fetchUser();
+      fetchUser("Christianfitz");
   }, []); // on the first page load
 
   return (
